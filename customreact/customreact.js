b7b652c6eb81fa70d7e65df4b7b88486721d3944
Asmit-Dhakal <asmitdhakal369@gmail.com>
2023-12-19 20:30:46 +0545

    push

--- customreact/customreact.js
+++ customreact/customreact.js
@@ -1,1 +1,23 @@
-const mainContainer =document.querySelector('#root')
+function customRender(reactElement,container){
+    /*
+    const domElement=document.createElement(reactElement.type)
+    domElement.innerHTML=reactElement.children
+    domElement.setAttribute('href',reactElement.props.href)
+    domElement.setAttribute('target',reactElement.props.target)
+    container.appendChild(domElement)
+    */
+   
+}
+
+const reactElement ={
+    type:'a',
+    props:{
+        href:"htpps://google.com" ,
+        target:'_blank'
+
+    },
+    children: 'Click me to visiit google'
+}
+
+const mainContainer =document.querySelector('#root') 
+customRender(reactElement,mainContainer)
