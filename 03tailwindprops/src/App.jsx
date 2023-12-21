8d442a859dc19237582c0c302937169728b559b0
Asmit-Dhakal <asmitdhakal369@gmail.com>
2023-12-21 15:49:03 +0545

    push

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -13,7 +13,7 @@ let myObj={
   return (
     <>
       <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind test</h1>
-      <Card channel='chai or code'/>
+      <Card channel='chai or code' someObject/>
       <Card />
     </>
   )
