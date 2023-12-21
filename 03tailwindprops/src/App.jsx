6be14630daae7f4ccb426defb8021c00f577e787
Asmit-Dhakal <asmitdhakal369@gmail.com>
2023-12-21 15:37:31 +0545

    push

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './component/Card'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,26 +10,7 @@ function App() {
   return (
     <>
       <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind test</h1>
-      <div class="relative h-[400px] w-[300px] rounded-md mb-4">
-  <img
-    src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
-    alt="AirMax Pro"
-    class="z-0 h-full w-full rounded-md object-cover"
-  />
-  <div class="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
-  <div class="absolute bottom-4 left-4 text-left">
-    <h1 class="text-lg font-semibold text-white">Delba</h1>
-    <p class="mt-2 text-sm text-gray-300">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
-      debitis?
-    </p>
-    <button class="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
-      View Profile →
-    </button>
-  </div>
-</div>
-
-
+      <Card/>
     </>
   )
 }
