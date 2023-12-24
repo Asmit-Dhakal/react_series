dbcc81911aae136af08154758bf820a3e069dba8
Asmit-Dhakal <asmitdhakal369@gmail.com>
2023-12-24 15:23:56 +0545

    hello

--- 03tailwindprops/src/component/Card.jsx
+++ 03tailwindprops/src/component/Card.jsx
@@ -16,8 +16,7 @@ import React from 'react'
     <div className="absolute bottom-4 left-4 text-left">
       <h1 className="text-lg font-semibold text-black">{username}</h1>
       <p className="mt-2 text-sm text-gray-300">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
-        debitis?
+        Hello EveryBody
       </p>
       <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-black">
       {btnText}
