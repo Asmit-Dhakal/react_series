10da423c1f44429037a4e971b917eecb2f582c54
Asmit-Dhakal <asmitdhakal369@gmail.com>
2023-12-24 12:41:51 +0545

    push

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -10,10 +10,11 @@ let myObj={
   username:"Asmit",
   age:21
 }
+let newArray=[1,2,3,4]
   return (
     <>
       <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind test</h1>
-      <Card channel='chai or code' someObject/>
+      <Card channel='chai or code' someObject={newArray}/>
       <Card />
     </>
   )
