d86ca93292544ca04a37236815f861fc0a0d423c
Asmit-Dhakal <asmitdhakal369@gmail.com>
2023-12-24 15:24:38 +0545

    push

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -15,7 +15,7 @@ let newArray=[1,2,3,4]
     <>
       <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind test</h1>
       <Card username='chai or code' btnText='Click Me'/>
-      <Card username='Asmit dhakal' btn Text='Visit Me'/>
+      <Card username='Asmit dhakal' btn Text='Call  Me'/>
     </>
   )
 }
